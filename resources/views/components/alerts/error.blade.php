0b7b78be736789c20f4d78286d763163683b3406
Richard Hofmänner <hello@terillos.dev>
2021-03-31 11:55:30 +0200

    Added error alert into register form

---
+++ resources/views/components/alerts/error.blade.php
@@ -0,0 +1,3 @@
+<div class="mb-4 bg-red-800 text-white p-3">
+    {{ $slot }}
+</div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -8,16 +8,18 @@
   <x-box-large>
     <img id="logo-login" src="{{ asset('img/logo-white.png') }}" alt="">
     @if ($errors->any())
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>{{ $error }}</li>
-        @endforeach
-      </ul>
+      <x-alerts.error>
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </x-alerts.error>
     @endif
     <form action="{{ route('register') }}" method="post">
       @csrf
       <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 mt-5">
-        <x-forms.input placeholder="First Name *" name="firstname" value="{{ old('name') }}" autofocus required />
+        <x-forms.input placeholder="First Name *" name="firstname" value="{{ old('firstname') }}" autofocus required />
         <x-forms.input placeholder="Last Name *" name="lastname" value="{{ old('lastname') }}" required />
         <x-forms.input placeholder="Email *" name="email" type="email" value="{{ old('email')  }}" required />
         <x-forms.input placeholder="Email confirmation *" name="email_confirmation" type="email" value="{{ old('email_confirmation') }}" required />
